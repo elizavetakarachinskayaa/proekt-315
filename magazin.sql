30ea024055e2f53f3395ed2a45a1a961cb25baae
Elizaveta Karachinskaya <elizavetakarachinskayaa@gmail.com>
2016-05-24 07:10:32 +0300

    second commit

--- magazin.sql
+++ magazin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 19 2016 г., 16:55
+-- Время создания: Май 24 2016 г., 08:08
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.2.12
 
@@ -23,48 +23,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `gorod`
+-- Структура таблицы `category_citys`
 --
 
-CREATE TABLE IF NOT EXISTS `gorod` (
+CREATE TABLE IF NOT EXISTS `category_citys` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `ID goroda` int(10) NOT NULL,
-  `adres` varchar(255) NOT NULL,
-  `telefon` char(12) NOT NULL,
-  `proezd` varchar(255) NOT NULL,
-  `opisanie` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`,`ID goroda`,`adres`,`telefon`,`proezd`,`opisanie`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
---
--- Дамп данных таблицы `gorod`
---
-
-INSERT INTO `gorod` (`ID`, `ID goroda`, `adres`, `telefon`, `proezd`, `opisanie`) VALUES
-(1, 1, 'Санкт-Петербург, Разъезжая, 42', '89123456789', 'Метро «Владимирская».Выйти из метро.Повернуть направо на Кузнечный переулок и пройти до пересечения с ул. Марата.Повернуть направо и пройти по ул. Марата до пересечения с ул. Разъезжая. Повернуть налево. Через 70 метров - магазин «Женские штучки».', 'Огромный выбор товаров'),
-(2, 2, 'Москва ул. Ленинская Слобода, 19', '89213457656', 'м. Автозаводская, ул. Ленинская Слобода, д.19, Бизнес Центр «Омега Плаза», 2 этаж  налево, офис сети магазинов "Женские штучки" ', 'Огромный выбор товаров по низким ценам'),
-(3, 3, 'Ярославль,  ул Чкалова, 14', '89603452113', 'Выйти к магазину «Марьинский Пассаж». Сесть на бесплатный автобус, следующий до ТЦ «Вегас». Интервал движения автобуса 45 мин., буд. 9:00-23:30, вых. 9:30-00:15.45 мин., буд. 9:00-23:30, вых. 9:30-00:15. Внутри ТЦ: 3й вход, 1 этаж, рядом с магазином МТС', 'Самые низкие цены в сети магазинах "Женские штучки"');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `kategorii_gorodov`
---
-
-CREATE TABLE IF NOT EXISTS `kategorii_gorodov` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `gorod` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`,`gorod`,`url`)
+  UNIQUE KEY `ID` (`ID`,`name`,`url`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Дамп данных таблицы `kategorii_gorodov`
+-- Дамп данных таблицы `category_citys`
 --
 
-INSERT INTO `kategorii_gorodov` (`ID`, `gorod`, `url`) VALUES
+INSERT INTO `category_citys` (`ID`, `name`, `url`) VALUES
 (1, 'Санкт-Петебург', 'SaintPetersburg'),
 (2, 'Москва', 'Moscow'),
 (3, 'Ярославль', 'Yaroslavl');
@@ -72,65 +46,51 @@ INSERT INTO `kategorii_gorodov` (`ID`, `gorod`, `url`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `kategorii_tovarov`
+-- Структура таблицы `city`
 --
 
-CREATE TABLE IF NOT EXISTS `kategorii_tovarov` (
+CREATE TABLE IF NOT EXISTS `city` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `url` varchar(255) DEFAULT NULL,
+  `ID_city` int(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `telefon` char(12) NOT NULL,
+  `driveway` varchar(255) NOT NULL,
+  `characteristic` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`,`url`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+  UNIQUE KEY `ID` (`ID`,`ID_city`,`address`,`telefon`,`driveway`,`characteristic`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Дамп данных таблицы `kategorii_tovarov`
+-- Дамп данных таблицы `city`
 --
 
-INSERT INTO `kategorii_tovarov` (`ID`, `name`, `url`) VALUES
-(1, 'Духи', 'duxi'),
-(2, 'Пудра', 'pudra'),
-(3, 'Румяна', 'rumayna'),
-(4, 'Тональное средства', 'tonalnoe_sredstvo'),
-(5, 'Корректор', 'korrektor'),
-(6, 'Тени', 'teni'),
-(7, 'Тушь', 'tush'),
-(8, 'Карандаш и лайнеры', 'karandach_lainer'),
-(9, 'Для бровей', 'dlay_brovei'),
-(10, 'Блеск для губ', 'blesk_gub'),
-(11, 'Помада', 'pomada'),
-(12, 'Карандаш для губ', 'karandash_gub'),
-(13, 'Дизайн ногтей', 'dizain_nogtei'),
-(14, 'Лак для ногтей', 'lak_nogti'),
-(15, 'Уход за ногтями', 'yxod_nogti'),
-(16, 'Средства для снятия лака', 'snaytie_laka'),
-(17, 'Пилочки для ногтей', 'piloshki_nogti'),
-(18, 'Лак для волос', 'lak_voloci'),
-(19, 'Аксессуары для волос', 'aksessyari_voloci'),
-(20, 'Окрашивание ', 'okrachivanie');
+INSERT INTO `city` (`ID`, `ID_city`, `address`, `telefon`, `driveway`, `characteristic`) VALUES
+(1, 1, 'Санкт-Петербург, Разъезжая, 42', '89123456789', 'Метро «Владимирская».Выйти из метро.Повернуть направо на Кузнечный переулок и пройти до пересечения с ул. Марата.Повернуть направо и пройти по ул. Марата до пересечения с ул. Разъезжая. Повернуть налево. Через 70 метров - магазин «Женские штучки».', 'Огромный выбор товаров'),
+(2, 2, 'Москва ул. Ленинская Слобода, 19', '89213457656', 'м. Автозаводская, ул. Ленинская Слобода, д.19, Бизнес Центр «Омега Плаза», 2 этаж  налево, офис сети магазинов "Женские штучки" ', 'Огромный выбор товаров по низким ценам'),
+(3, 3, 'Ярославль,  ул Чкалова, 14', '89603452113', 'Выйти к магазину «Марьинский Пассаж». Сесть на бесплатный автобус, следующий до ТЦ «Вегас». Интервал движения автобуса 45 мин., буд. 9:00-23:30, вых. 9:30-00:15.45 мин., буд. 9:00-23:30, вых. 9:30-00:15. Внутри ТЦ: 3й вход, 1 этаж, рядом с магазином МТС', 'Самые низкие цены в сети магазинах "Женские штучки"');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `tovar`
+-- Структура таблицы `products`
 --
 
-CREATE TABLE IF NOT EXISTS `tovar` (
+CREATE TABLE IF NOT EXISTS `products` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `ID kategorii` int(10) NOT NULL,
+  `ID_category` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` double NOT NULL,
-  `kolichestvo` int(10) NOT NULL,
-  `opisanie` varchar(255) NOT NULL,
+  `count` int(10) NOT NULL,
+  `characteristic` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
 
 --
--- Дамп данных таблицы `tovar`
+-- Дамп данных таблицы `products`
 --
 
-INSERT INTO `tovar` (`ID`, `ID kategorii`, `name`, `price`, `kolichestvo`, `opisanie`) VALUES
+INSERT INTO `products` (`ID`, `ID_category`, `name`, `price`, `count`, `characteristic`) VALUES
 (1, 1, 'Парфюмерная вода VERSACE BRIGHT CRYSTAL ABSOLU', 3050, 100, 'VERSACE представляет новый аромат BRIGHT CRYSTAL ABSOLU - чистая чувственность, кристальная прозрачность и сияние.'),
 (2, 1, 'ПАРФЮМЕРНАЯ ВОДА TRUSSARDI MY NAME', 2850, 100, 'Чувственный женский аромат TRUSSARDI MY NAME нотами белой фиалки и сирени, в сочетании с ванилью отражает соблазнительную, уверенную в себе женщину.'),
 (3, 1, 'ПАРФЮМЕРНАЯ ВОДА ALAN BRAY ВЫСШИЙ СВЕТ ECLAT D ETOILE', 625, 50, 'Аромат открывается свежим  аккордом мандарина в обрамлении жасмина, розы, орхидеи и ландыша, подчеркнутых прелестным ежевично-мускусным шлейфом'),
@@ -164,6 +124,46 @@ INSERT INTO `tovar` (`ID`, `ID kategorii`, `name`, `price`, `kolichestvo`, `opis
 (31, 19, 'АКСЕССУАР ДЛЯ ВОЛОС LADY PINK', 900, 600, 'Хорошо держит волосы'),
 (32, 20, 'КРАСКА ДЛЯ ВОЛОС GARNIER COLOR & SHINE', 700, 300, 'ТОН 5.5 (КРАСНОЕ ДЕРЕВО)'),
 (33, 20, 'КРАСКА ДЛЯ ВОЛОС SYOSS MIXING COLORS', 7000, 400, 'ТОН 1-18 (ГОРЬКИЙ ШОКОЛАД МИКС) 60 МЛ');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `сategory_products`
+--
+
+CREATE TABLE IF NOT EXISTS `сategory_products` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`,`url`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+--
+-- Дамп данных таблицы `сategory_products`
+--
+
+INSERT INTO `сategory_products` (`ID`, `name`, `url`) VALUES
+(1, 'Духи', 'duxi'),
+(2, 'Пудра', 'pudra'),
+(3, 'Румяна', 'rumayna'),
+(4, 'Тональное средства', 'tonalnoe_sredstvo'),
+(5, 'Корректор', 'korrektor'),
+(6, 'Тени', 'teni'),
+(7, 'Тушь', 'tush'),
+(8, 'Карандаш и лайнеры', 'karandach_lainer'),
+(9, 'Для бровей', 'dlay_brovei'),
+(10, 'Блеск для губ', 'blesk_gub'),
+(11, 'Помада', 'pomada'),
+(12, 'Карандаш для губ', 'karandash_gub'),
+(13, 'Дизайн ногтей', 'dizain_nogtei'),
+(14, 'Лак для ногтей', 'lak_nogti'),
+(15, 'Уход за ногтями', 'yxod_nogti'),
+(16, 'Средства для снятия лака', 'snaytie_laka'),
+(17, 'Пилочки для ногтей', 'piloshki_nogti'),
+(18, 'Лак для волос', 'lak_voloci'),
+(19, 'Аксессуары для волос', 'aksessyari_voloci'),
+(20, 'Окрашивание ', 'okrachivanie');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
